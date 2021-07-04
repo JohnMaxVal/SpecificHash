621a7145eb2c02a8e47e34974013b789c0065f19
Eugene M <darkstarmacho@gmail.com>
2021-07-04 20:43:41 +0300

    Refactored class and add basec input process functionality

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
-EXE_NAME = hash
+EXE_NAME = main
 
 CC = g++
-CC_FLAGS = -g
+CC_FLAGS = -g -std=c++11 -O2 -Wall
 
+build:
+	$(CC) $(CC_FLAGS) main.cpp -o $(EXE_NAME)
+
+run:
+	./$(EXE_NAME)
--- main.cpp
+++ main.cpp
@@ -1,17 +1,57 @@
+#include <iostream>
+#include <sstream>
+#include <string>
 
-namespace {
+using namespace std;
 
-    class Hash {
-        public:
-            Hash();
-            virtual ~Hash();
-        
+class Hashtable {
+
+private:
+    struct Entry {
+        string data;
+        string status;
     };
 
-}
+public:
+    const static int size = 26;
+    Entry m_entries[size];
 
+public:
+    Hashtable() {
+        for(int i = 0; i < size; ++i)
+            m_entries[i].status = "never used";
+    }
+
+    void print() {
+        for(int i = 0; i < size; ++i)
+            cout << m_entries[i].data << " " << "(" << m_entries[i].status << ")" << endl;
+    }
+
+};
+
+// input: Aapple Aorange Dapple Astrawberry
+// output: orange strawberry
 
 int main() {
-    Hash hash;
+    Hashtable table;
+    //table.print();
+
+    string input;
+    getline(cin, input);
+
+    cout << "Input line: " << input << endl;
+
+    stringstream ss(input);
+    string token;
+    
+    while(ss >> token) {
+        
+        if(token[0] == 'A')
+            cout << &token[1] << endl;
+        else if(token[0] == 'D')
+            cout << &token[1] << endl;
+            
+    }
+
     return 0;
 }
