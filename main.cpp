a9ed21a0268dfbf47b4aed869fd2aa21ba3a45a1
Eugene M <darkstarmacho@gmail.com>
2021-07-04 22:20:43 +0300

    Implement add function

--- main.cpp
+++ main.cpp
@@ -22,9 +22,55 @@ public:
             m_entries[i].status = "never used";
     }
 
+    void insert(const string& value) {
+        bool found = search(value);
+
+        if(!found) {
+            int index = getInsertIndex(value);
+            m_entries[index].data = value;
+            m_entries[index].status = "occupied";
+        }
+    }
+
+    void remove(const string& value) {
+        bool found = search(value);
+    }
+
     void print() {
         for(int i = 0; i < size; ++i)
             cout << m_entries[i].data << " " << "(" << m_entries[i].status << ")" << endl;
+    }
+
+private:
+
+    bool search(const string value) {
+        int index = getTableIndex(value);
+        
+        for(;;) {
+
+            if(m_entries[index].data == value)
+                return true;
+
+            if(m_entries[index].status == "never used")
+                return false;
+            
+            index = (index + 1) % 25;
+        }
+    }
+
+    int getTableIndex(const string& input) {
+        return input.back() - 'a';
+    }
+
+    int getInsertIndex(const string& input) {
+        int index = getTableIndex(input);
+
+        for(;;) {
+            if(m_entries[index].status == "never used" || m_entries[index].status == "tombstone")
+                return index;
+
+            index = (index + 1) % 25;
+        }
     }
 
 };
@@ -34,7 +80,6 @@ public:
 
 int main() {
     Hashtable table;
-    //table.print();
 
     string input;
     getline(cin, input);
@@ -45,13 +90,17 @@ int main() {
     string token;
     
     while(ss >> token) {
-        
+
+        cout << "token: " << token << endl;
+
         if(token[0] == 'A')
-            cout << &token[1] << endl;
+            table.insert(&token[1]);
         else if(token[0] == 'D')
-            cout << &token[1] << endl;
+            table.remove(&token[1]);
             
     }
+
+    table.print();
 
     return 0;
 }
